7d828c22f9cdd442a8f1ad942fe198cb975d1272
mazn_glb <mazn_gbl@git.com>
2020-07-17 18:01:23 +0800

    springCloud2020项目，构建消费者模块，实现消费者模块通过http调用支付模块，并与eureka整合

---
+++ cloud-consumer-order-80/src/main/java/com/mazn/springcloud/ConsumerOrderMain80.java
@@ -0,0 +1,13 @@
+package com.mazn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConsumerOrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerOrderMain80.class,args);
+    }
+}
